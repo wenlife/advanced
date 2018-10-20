236c6bc8d4d77a6e81cd38db4b6803092f251499
wenlife <mawl146@qqcom>
2018-10-20 12:49:34 +0800

    Signed-off-by: wenlife <mawl146@qqcom>

---
+++ console/migrations/m170221_122155_20170221.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170221_122155_20170221 extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170221_122155_20170221 cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
