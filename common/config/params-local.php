bce6196100df4af9804f7c4544c81178217d4000
wenlife207-office <mawl146@qq.com>
2018-10-25 10:44:59 +0800

    remove some gitignore

---
+++ common/config/params-local.php
@@ -0,0 +1,35 @@
+<?php
+return [
+	
+	'itemType'=>[
+			0=>"综合",
+			1=>"文章",
+			2=>"视频",
+			3=>"图片",
+			4=>"轮播",
+	],
+	'itemModelMap'=>[
+			0=>'content_infomation',
+			1=>'content_infomation',
+			2=>'content_infomation',
+			3=>'content_infomation',
+			4=>'content_infomation',
+	],
+
+	'subject'=>[
+			'yw'=>'语文',
+			'ds'=>'数学',
+			'yy'=>'英语',
+			'wl'=>'物理',
+			'hx'=>'化学',
+			'sw'=>'生物',
+			'zz'=>'政治',
+			'ls'=>'历史',
+			'dl'=>'地理',
+			'ty'=>'体育',
+			'xx'=>'信息技术',
+			'ty'=>'通用技术',
+
+	],
+
+];
